f370d0fc2853ec4fb0078e774f407612f2e1a525
Evan <evan.schubert@selu.edu>
2020-11-09 12:24:14 -0600

    11/9

---
+++ src/Admin.js
@@ -0,0 +1,15 @@
+import React,{useState, useContext} from 'react';
+import './index.css';
+import {Link} from 'react-router-dom';
+
+
+
+function Admin() {
+  return (
+    <div>
+      <h1> Admin</h1>
+       </div>
+  );
+}
+
+export default Admin;
